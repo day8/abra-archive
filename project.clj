9f4c81b0aaf36ec07e478350bc66dbcebb5ec06e
Stuart Mitchell <stu@stuartmitchell.com>
2014-12-19 14:30:59 +1300

    moved cljs-asynchronize to clojars

--- project.clj
+++ project.clj
@@ -8,20 +8,18 @@
                  [cljs-ajax "0.2.4"]
                  [reagent "0.4.2"]
                  [re-com "0.1.6"]
-                 [re-frame "0.1.0"]]
+                 [re-frame "0.1.0"]
+                 [cljs-asynchronize "0.1.1-SNAPSHOT"]]
   
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
-            [lein-git-deps "0.0.1-SNAPSHOT"]
             [com.cemerick/clojurescript.test "0.3.1"]]
   
   :profiles {:dev {:plugins [[com.cemerick/clojurescript.test "0.3.1"]]}}
   
-  :git-dependencies [["https://github.com/gilbertw1/cljs-asynchronize.git"]]
-  
   :jvm-opts         ["-Xmx1g" "-XX:+UseConcMarkSweepGC"] ;; cljsbuild eats memory
   
   :cljsbuild {:builds [{:id "main"
-                        :source-paths ["src/main" ".lein-git-deps/cljs-asynchronize/src"]
+                        :source-paths ["src/main"]
                         :compiler {
                                    :output-to  "run/js/compiled/main.js"
                                    :source-map "run/js/compiled/main.js.map"
@@ -40,7 +38,7 @@
                                    :pretty-print true}}
                        
                        {:id "test-node" 
-                        :source-paths ["src/main/backend" "test" ".lein-git-deps/cljs-asynchronize/src"]
+                        :source-paths ["src/main/backend" "test"]
                         ; :notify-command ["node" "run/test/bin/runner-node.js" 
                         ;                     "run/js/compiled/test"
                         ;                     "run/js/compiled/test_node.js"]
