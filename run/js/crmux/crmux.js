72cf7e5ab51500e108ff4cc3eb674a33b683b699
Stuart Mitchell <github@stuartmitchell.com>
2015-06-04 10:03:29 +1200

    made crmux the same as the github fix

--- run/js/crmux/crmux.js
+++ run/js/crmux/crmux.js
@@ -139,15 +139,14 @@ function start_crmux_server(config) {
                       s.send(message);
                   });
               } else {
-                  var local = msgObj.id;
-                  var idMap = upstreamMap[wsUpstreamUrl].localIdToRemote[local];
+                  var idMap = upstreamMap[wsUpstreamUrl].localIdToRemote[msgObj.id];
+                  delete upstreamMap[wsUpstreamUrl].localIdToRemote[msgObj.id];
                   msgObj.id = idMap.id;
                   idMap.client.send(JSON.stringify(msgObj));
                   if (config.debug) {
                       console.log(String(idMap.client._id).blue + "> " + idMap.message.yellow);
                       console.log(String(idMap.client._id).blue + "> " + JSON.stringify(msgObj).green);
                   }
-                  delete upstreamMap[wsUpstreamUrl].localIdToRemote[local];
               }
           });
       } else {
